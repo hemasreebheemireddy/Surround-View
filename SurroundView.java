3b6684c5da8458c1ab91b6e8f62948cd99cc8fec
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 15:15:20 +0530

    #1204 SurroundView.java
    The system captures video streams from multiple cameras mounted around the vehicle (e.g., front, rear, sides)

---
+++ SurroundView.java
@@ -0,0 +1,9 @@
+public class ObstacleDetection {
+    public static void main(String[] args) {
+        System.out.println("Detected obstacles:");
+        System.out.println("Pedestrian");
+        System.out.println("Vehicle");
+        System.out.println("Wall");
+    }
+}
+ 
